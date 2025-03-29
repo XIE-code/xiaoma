03afad24f33505723a9f71390910bbd1d040bc4c
XIE <1123920263@qq.com>
2025-03-29 09:00:24 +0800

    修改签名控件灵敏问题

---
+++ .history/pages.config_20250327163734.ts
@@ -0,0 +1,56 @@
+import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
+
+export default defineUniPages({
+  globalStyle: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '小马养梯',
+    navigationBarBackgroundColor: '#f8f8f8',
+    navigationBarTextStyle: 'black',
+    backgroundColor: '#FFFFFF',
+  },
+  easycom: {
+    autoscan: true,
+    custom: {
+      '^wd-(.*)': 'wot-design-uni/components/wd-$1/wd-$1.vue',
+      '^(?!z-paging-refresh|z-paging-load-more)z-paging(.*)':
+        'z-paging/components/z-paging$1/z-paging$1.vue',
+    },
+  },
+  tabBar: {
+    custom: true,
+    color: '#8A8A8A',
+    selectedColor: '#539DF3',
+    backgroundColor: '#F8F8F8',
+    borderStyle: 'black',
+    height: '50px',
+    fontSize: '10px',
+    iconWidth: '24px',
+    spacing: '3px',
+    list: [
+      {
+        iconPath: 'static/tabbar/home.png',
+        selectedIconPath: 'static/tabbar/home-select.png',
+        pagePath: 'pages/index/index',
+        text: '首页',
+      },
+      {
+        iconPath: 'static/tabbar/maintenance.png',
+        selectedIconPath: 'static/tabbar/maintenance-select.png',
+        pagePath: 'pages/maintenance/maintenance',
+        text: '保养',
+      },
+      {
+        iconPath: 'static/tabbar/forewarning.png',
+        selectedIconPath: 'static/tabbar/forewarning-select.png',
+        pagePath: 'pages/forewarning/forewarning',
+        text: '急修',
+      },
+      {
+        iconPath: 'static/tabbar/personal.png',
+        selectedIconPath: 'static/tabbar/personal-select.png',
+        pagePath: 'pages/personal/personal',
+        text: '我的',
+      },
+    ],
+  },
+})
---
+++ .history/src/store/user_20250321135755.ts
@@ -0,0 +1,55 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+import { ILoginResponse } from '@/service/login/type'
+
+const initState: ILoginResponse = {
+  userId: 0, // 用户ID
+  token: '', // 访问令牌
+  companyId: 0, // 公司ID
+  userName: '', // 用户名
+  email: '', // 邮箱
+  realname: '', // 姓名
+  phone: '', // 电话
+  officePhone: '', // 工作电话
+  sex: 0, // 性别
+  identity: '', // 身份
+  headImg: '', // 头像
+  jobs: 0, // 职位
+  roleId: 0, // 角色ID
+  role: 0, // 角色
+  companyType: 0, // 公司类型
+  companyName: '', // 公司名称
+}
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const userInfo = reactive<ILoginResponse>({ ...initState })
+
+    const setUserInfo = (val: ILoginResponse) => {
+      Object.assign(userInfo, val)
+    }
+
+    const resetUserInfo = () => {
+      Object.assign(userInfo, initState)
+    }
+
+    const getUserInfo = () => {
+      return userInfo
+    }
+
+    const getToken = () => {
+      return userInfo.token
+    }
+
+    return {
+      userInfo,
+      getUserInfo,
+      setUserInfo,
+      resetUserInfo,
+      getToken,
+    }
+  },
+  {
+    persist: true,
+  },
+)
